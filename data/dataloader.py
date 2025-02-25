corrected kl kfac

data/dataloader.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 
 
-def get_bMNIST(batch_size):
+def get_bMNIST(precision, batch_size):
     transform = transforms.Compose([
         transforms.ToTensor(), 
     ])
@@ -17,8 +17,13 @@ def get_bMNIST(batch_size):
     # Tranform into binary MNIST
     # Classes 0, ..., 4 maps to 0
     # Classes 5, ..., 9 maps to 1
-    trainset.targets = (trainset.targets >= 5).type(torch.float32)
-    testset.targets = (testset.targets >= 5).type(torch.float32)
+
+    if precision == "float32":
+        trainset.targets = (trainset.targets >= 5).type(torch.float32)
+        testset.targets = (testset.targets >= 5).type(torch.float32)
+    else:
+        trainset.targets = (trainset.targets >= 5).type(torch.float64)
+        testset.targets = (testset.targets >= 5).type(torch.float64)
     
     trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
     testloader = DataLoader(testset, batch_size=batch_size, shuffle=False)

models/bayes_linear.py
@@ -14,7 +14,8 @@ class BayesianLinear(nn.Module):
                  init_q: tuple[torch.Tensor, torch.Tensor],
                  id: int,
                  approx="diagonal",
-                 init_value=1e-2):
+                 init_value=1e-2, 
+                 precision="float32"):
         """Bayesian Linear Layer.
 
         Args:
@@ -38,6 +39,7 @@ class BayesianLinear(nn.Module):
 
         self.in_features = in_features
         self.out_features = out_features
+        self.precision = precision
 
         # init prior
         ##elf.p_weight_mu = nn.Parameter(init_p["weight"], requires_grad=False)
@@ -47,7 +49,10 @@ class BayesianLinear(nn.Module):
         # for use in optimizer. will store gradients and A, G for momentum updates
         self.id = id
 
-        self.p_mu = nn.Parameter(torch.cat((init_p["weight"], init_p['bias'].unsqueeze(1)), dim=1), requires_grad=False)  
+        self.p_mu = nn.Parameter(
+                torch.cat((init_p["weight"].to(getattr(torch, precision)), init_p['bias'].unsqueeze(1).to(getattr(torch, precision))), dim=1), 
+                requires_grad=False
+            )  
         self.q_bias_mu = None
 
         self.training = True
@@ -59,46 +64,45 @@ class BayesianLinear(nn.Module):
         # TODO: does not currently work
         if self.approx == 'noisy-kfac':
             # F \approx A \otimes G
-            self._A = init_value * torch.eye(self.in_features + 1)  #+ regularization * torch.eye(self.in_features)
-            self._G = init_value * torch.eye(self.out_features) #+ regularization * torch.eye(self.out_features)\
+            self._A = init_value * torch.eye(self.in_features + 1, dtype=getattr(torch, precision))  #+ regularization * torch.eye(self.in_features)
+            self._G = init_value * torch.eye(self.out_features, dtype=getattr(torch, precision)) #+ regularization * torch.eye(self.out_features)\
 
-            self.A_inv, self.G_inv = 1/init_value * torch.eye(self.in_features + 1), 1/init_value * torch.eye(self.out_features)
+            self.A_inv = 1/init_value * torch.eye(self.in_features + 1, dtype=getattr(torch, precision))
+            self.G_inv = 1/init_value * torch.eye(self.out_features, dtype=getattr(torch, precision))
             # initialize weights as MLP weights (we will compute gradients wrt weights)
-            self.weights =  nn.Parameter(init_q["weight"])
-            self.q_mu = nn.Parameter(torch.cat((init_q["weight"], init_q['bias'].unsqueeze(1)), dim=1), requires_grad=False) 
+            self.weights =  nn.Parameter(init_q["weight"].to(torch.float64))
+            self.q_mu = nn.Parameter(torch.cat((init_q["weight"].to(getattr(torch, precision)), 
+                                                init_q['bias'].unsqueeze(1).to(getattr(torch, precision))), dim=1), 
+                                     requires_grad=False
+                                     ) 
 
             
         elif self.approx == 'kfac':
             # we include the bias term in A
             self._A = init_value * torch.eye(self.in_features + 1)
             self._G = init_value * torch.eye(self.out_features)
-            self.q_mu = nn.Parameter(torch.cat((init_q["weight"], init_q['bias'].unsqueeze(1)), dim=1)) 
+            self.q_mu = nn.Parameter(torch.cat((init_q["weight"].to(getattr(torch, precision)), 
+                                                init_q['bias'].unsqueeze(1).to(getattr(torch, precision))), dim=1)) 
 
         else:
             self.q_mu = nn.Parameter(torch.cat((init_q["weight"], init_q['bias'].unsqueeze(1)), dim=1)) 
-            self.q_log_sigma = nn.Parameter(torch.cat((0.5 * torch.log(torch.abs(init_q["weight"])), \
-                                                   0.5 * torch.log(torch.abs(init_q["bias"])).unsqueeze(1)), dim=1)) 
+            self.q_log_sigma = nn.Parameter(torch.cat((0.5 * torch.log(torch.abs(init_q["weight"].to(getattr(torch, precision)))), \
+                                                   0.5 * torch.log(torch.abs(init_q["bias"])).unsqueeze(1).to(getattr(torch, precision))), dim=1)) 
             self.q_bias_mu = None
             
 
 
-    def forward(self, x, p_log_sigma, flag, T_stats=20, beta_1=0.9, num_samples=5):
-        """
-            params:
-                - k: current iteration number
-                - T_stats: frequency of updating A
-        """
-        kl, outputs = None, None
+    def forward(self, x, p_log_sigma, flag, num_samples=5):
         if self.approx == 'noisy-kfac':
-            x = torch.cat([x, torch.ones(x.size(0), 1)], dim=1)
+            x = torch.cat([x, torch.ones(x.size(0), 1)], dim=1).type(getattr(torch, self.precision))
             # prior std
             p_sigma = torch.exp(p_log_sigma)
 
             kl = self.kl_divergence_kfac(self.p_mu, p_sigma, self.q_mu, flag)
 
             # sample the weights from MN(q_mu, lambda/N A^{-1}, G^{-1})
-            self.weights.data = self.q_mu#current_sampling(self.q_mu, self.A_inv, self.G_inv).view(self.out_features, self.in_features + 1)
-            outputs = F.linear(x, self.weights, torch.zeros(self.out_features))
+            self.weights.data = current_sampling(self.q_mu, self.A_inv, self.G_inv).view(self.out_features, self.in_features + 1)
+            outputs = F.linear(x, self.weights, torch.zeros(self.out_features).type(getattr(torch, self.precision)))
         
         # kfactored posterior approximation
         elif self.approx == 'kfac':
@@ -133,7 +137,7 @@ class BayesianLinear(nn.Module):
             weights = self.q_mu + q_sigma * torch.randn_like(q_sigma)
             kl = self.kl_normal_diag(self.p_mu, p_sigma, self.q_mu, q_sigma)
 
-            outputs = F.linear(x, weights, torch.zeros(self.out_features))
+            outputs = F.linear(x, weights, torch.zeros(self.out_features).type(getattr(torch, self.precision)))
 
         return outputs, kl
     
@@ -158,6 +162,7 @@ class BayesianLinear(nn.Module):
         
         return kl
     
+
     def kl_divergence_kfac(self, p_mu, p_sigma, q_weight_mu, flag, epsilon=1e-1):
         #NOTE: make sure the eps aligns with clamping param in kfac/noisy-kfac
         """
@@ -175,6 +180,7 @@ class BayesianLinear(nn.Module):
         # Retrieve the KFAC blocks from this class
         A, G = self._A, self._G
         n, m = A.shape[0], G.shape[0]
+        
 
         
         #trace_A = torch.trace(A)
@@ -211,16 +217,19 @@ class BayesianLinear(nn.Module):
         
         # trace_term = trace(A) * trace(G) / p_sigma^2
         # but trace(A) = sum(dA), trace(G) = sum(dG)
-        trace_term = torch.trace(A_inv) * torch.trace(G_inv) / (p_sigma**2)
+        trace_term = torch.trace(A_inv) * torch.trace(G_inv) * 1.0/(p_sigma**2)
         
         # (q_weight_mu - p_weight_mu) is shaped (m*n,)
-        delta_mu = q_weight_mu - p_mu
+        #delta_mu = q_weight_mu - p_mu
         # Reshape to (m, n) to match the Kronecker structure
-        delta_mu_reshaped = delta_mu.view(m, n)
+        #delta_mu_reshaped = delta_mu.view(m, n)
         
         # Quadratic term = trace(G_inv @ (delta_mu @ A_inv @ delta_mu^T))
-        inside = delta_mu_reshaped @ A_inv @ delta_mu_reshaped.T
-        quadratic_term = torch.trace(G_inv @ inside)
+        #inside = delta_mu_reshaped @ A_inv @ delta_mu_reshaped.T
+        #quadratic_term = torch.trace(G_inv @ inside)
+        diff = q_weight_mu - p_mu
+        quad_term = (1.0 / (p_sigma**2)) * torch.sum(diff**2)
+
 
         #print("PSIGMA", p_sigma)
         #print(log_det_A, log_det_G, log_det_prior, trace_term, quadratic_term)
@@ -228,22 +237,25 @@ class BayesianLinear(nn.Module):
         # 0.5 * ((log_det_A + n*log_det_G - log_det_prior - m*n) 
         #        + trace_term + quadratic_term)
         kl = 0.5 * (
-            (m*log_det_A + n * log_det_G - log_det_prior - m * n)
+            (m * log_det_A + n * log_det_G + log_det_prior - m * n)
             + trace_term
-            + quadratic_term
+            + quad_term
         )
+        #print("HERE", n, m)
+        #print(log_det_A, log_det_G, log_det_prior, trace_term, quad_term)
         
         #print("KL:", kl)
-        print("Before rounding", kl)
+        #print("Before rounding", kl)
         # handling unstable KL 
         if kl < 0:
-            kl = 0
-            if flag == 'eval': kl = self.prev_kl
+            kl = torch.tensor(0)
+            #if flag == 'eval': kl = self.prev_kl
             #if self.training: self.training = False
-        else:
-            self.prev_kl = kl
+        #else:
+        #    self.prev_kl = kl
         
         #print(kl)
+        #print("KL", kl)
         return kl
     
     def kl_normal_diag(self, p_mu, p_sigma, q_mu, q_sigma):

models/bnn.py
@@ -13,7 +13,8 @@ class BayesianNN(nn.Module):
                  out_features: int = 1,
                  hidden_features: int = 300,
                  p_log_sigma: float = -1.16, #-6,
-                 approx="diagonal"):
+                 approx="diagonal",
+                 precision="float32"):
         """Bayesian Neural Network (BNN) model w/ 3 layers.
 
         Args:
@@ -33,16 +34,21 @@ class BayesianNN(nn.Module):
         self.approx = approx
         # TODO: fixed log sigma for now because it gets pushed too small if we let it vary
         # find closed form optimal?
-        self.p_log_sigma = nn.Parameter(torch.tensor(p_log_sigma, dtype=torch.float32), requires_grad =False)
+
+        if precision == "float32":
+            self.p_log_sigma = nn.Parameter(torch.tensor(p_log_sigma, dtype=torch.float32), requires_grad =False)
+        else:
+            self.p_log_sigma = nn.Parameter(torch.tensor(p_log_sigma, dtype=torch.float64), requires_grad =False)
+
         
 
         self.in_features = in_features
         self.out_features = out_features
         
         
-        self.bl1 = BayesianLinear(in_features, hidden_features, init_p_weights[0], init_q_weights[0], 1, approx=approx)
-        self.bl2 = BayesianLinear(hidden_features, hidden_features, init_p_weights[1], init_q_weights[1], 2, approx=approx)
-        self.bl3 = BayesianLinear(hidden_features, 1, init_p_weights[2], init_q_weights[2], 3, approx=approx)
+        self.bl1 = BayesianLinear(in_features, hidden_features, init_p_weights[0], init_q_weights[0], 1, approx=approx, precision=precision)
+        self.bl2 = BayesianLinear(hidden_features, hidden_features, init_p_weights[1], init_q_weights[1], 2, approx=approx, precision=precision)
+        self.bl3 = BayesianLinear(hidden_features, 1, init_p_weights[2], init_q_weights[2], 3, approx=approx, precision=precision)
 
         self.layers = [self.bl1, self.bl2, self.bl3]
         self.flag = "train" # updated for eval

optimizers/noisy_kfac.py
@@ -20,7 +20,8 @@ class NoisyKFAC(optim.Optimizer):
             T_stats=10,
             T_inv=100,
             gamma_ex=1e-3,
-            batch_averaged=True
+            batch_averaged=True,
+            precision="float32"
         ):
         params = [p for p in model.parameters() if p.requires_grad]
         defaults = dict(lr=lr, momentum=momentum, damping=damping,
@@ -45,6 +46,7 @@ class NoisyKFAC(optim.Optimizer):
         self.lam = lam
         self.N = N
         self.kl_clip = kl_clip
+        self.precision = precision
 
         # only for kfac-enabled BNNs
         #assert(model.kfac)
@@ -71,11 +73,11 @@ class NoisyKFAC(optim.Optimizer):
         if torch.is_grad_enabled() and self.steps % self.T_stats == 0:
             aa = self.CovAHandler(input[0].data, module)
             # give the model access to kfactors
-            module._A = aa
+            #module._A = self.lam/self.N * aa
             # Initialize buffers
             if self.steps == 0:
                 self.m_aa[module] = torch.diag(aa.new(aa.size(0)).fill_(1))
-            update_running_stat(aa, self.m_aa[module], self.beta)
+            update_running_stat(aa, self.m_aa[module], self.beta, module, "A")
 
     def _save_grad_output(self, module, grad_input, grad_output):
         """
@@ -88,11 +90,11 @@ class NoisyKFAC(optim.Optimizer):
         if self.acc_stats and self.steps % self.T_stats == 0:
             gg = self.CovGHandler(grad_output[0].data, module, self.batch_averaged)
             # give the model access to the kfactors
-            module._G = gg
+            #module._G = gg
             # Initialize buffers
             if self.steps == 0:
                 self.m_gg[module] = torch.diag(gg.new(gg.size(0)).fill_(1))
-            update_running_stat(gg, self.m_gg[module], self.beta)
+            update_running_stat(gg, self.m_gg[module], self.beta, module, "G")
 
     def _update_inv(self, layer, damping):
         """Do eigen decomposition for computing inverse of the ~ fisher.
@@ -107,6 +109,10 @@ class NoisyKFAC(optim.Optimizer):
         self.d_g[layer], self.Q_g[layer] = torch.linalg.eigh(
                 self.m_gg[layer], UPLO='U'
             )
+        self.d_a[layer] = self.d_a[layer].type(getattr(torch, self.precision))
+        self.d_g[layer] = self.d_g[layer].type(getattr(torch, self.precision))
+        self.Q_a[layer] = self.Q_a[layer].type(getattr(torch, self.precision))
+        self.Q_g[layer] = self.Q_g[layer].type(getattr(torch, self.precision))
 
         self.d_a[layer].mul_((self.d_a[layer] > eps).float())
         self.d_g[layer].mul_((self.d_g[layer] > eps).float())
@@ -169,6 +175,8 @@ class NoisyKFAC(optim.Optimizer):
         """
         # p_grad_mat is of output_dim * input_dim
         # inv((ss')) p_grad_mat inv(aa') = [ Q_g (1/R_g) Q_g^T ] @ p_grad_mat @ [Q_a (1/R_a) Q_a^T]
+        #(#self.Q_a[m] = self.Q_a[m].type(getattr(torch, self.precision))
+        #print(self.Q[m].dtype)
         v1 = self.Q_g[m].t() @ (p_grad_mat- self.lam/(self.N * self.eta) * m.weights) @ self.Q_a[m]
         v2 = v1 / (self.d_g[m].unsqueeze(1) * self.d_a[m].unsqueeze(0) + damping)
         v = self.Q_g[m] @ v2 @ self.Q_a[m].t()

train_kfac.py
@@ -15,46 +15,9 @@ from utils.pac_bayes_loss import pac_bayes_loss2
 from utils.evaluate import evaluate_BNN
 from utils.config import *
 import matplotlib.pyplot as plt
+from utils.args_parser import ArgsParser
 
-
-# fetch args
-parser = argparse.ArgumentParser()
-
-
-parser.add_argument('--network', default='vgg16_bn', type=str)
-parser.add_argument('--depth', default=19, type=int)
-parser.add_argument('--dataset', default='cifar10', type=str)
-
-# densenet
-parser.add_argument('--growthRate', default=12, type=int)
-parser.add_argument('--compressionRate', default=2, type=int)
-
-# wrn, densenet
-parser.add_argument('--widen_factor', default=1, type=int)
-parser.add_argument('--dropRate', default=0.0, type=float)
-
-
-parser.add_argument('--device', default='cuda', type=str)
-parser.add_argument('--resume', '-r', action='store_true')
-parser.add_argument('--load_path', default='', type=str)
-parser.add_argument('--log_dir', default='runs/pretrain', type=str)
-
-
-parser.add_argument('--optimizer', default='kfac', type=str)
-parser.add_argument('--batch_size', default=64, type=float)
-parser.add_argument('--epoch', default=1, type=int)
-parser.add_argument('--milestone', default=None, type=str)
-parser.add_argument('--learning_rate', default=0.001, type=float)
-parser.add_argument('--momentum', default=0.9, type=float)
-parser.add_argument('--stat_decay', default=0.95, type=float)
-parser.add_argument('--damping', default=1e-3, type=float)
-parser.add_argument('--kl_clip', default=1e-2, type=float)
-parser.add_argument('--weight_decay', default=3e-3, type=float)
-parser.add_argument('--T_stats', default=10, type=int)
-parser.add_argument('--TScal', default=10, type=int)
-parser.add_argument('--T_inv', default=100, type=int)
-
-parser.add_argument('--prefix', default=None, type=str)
+parser = ArgsParser()
 args = parser.parse_args()
 
 # init model
@@ -67,12 +30,12 @@ nc = {
 num_classes = nc[args.dataset]
 
 # init dataloader
-trainloader, testloader = get_bMNIST(batch_size=100)
+trainloader, testloader = get_bMNIST(args.precision, batch_size=100)
 
 w0 = torch.load('./checkpoints/mlp/w0.pt', weights_only=True)
 w1 = torch.load('./checkpoints/mlp/w1.pt', weights_only=True)
 
-net = BayesianNN(w0, w1, p_log_sigma=-1.16,  approx='noisy-kfac').to(device)
+net = BayesianNN(w0, w1, p_log_sigma=-1.16,  approx='noisy-kfac', precision=args.precision).to(device)
 # init optimizer and lr scheduler
 optim_name = args.optimizer.lower()
 tag = optim_name
@@ -85,7 +48,7 @@ elif optim_name == 'kfac':
     # optimal params for diagonal
     #optimizer = KFACOptimizer(net, lr=0.019908763029878117, damping=0.09398758455968932, weight_decay=0)
     optimizer = NoisyKFAC(net, T_stats=10, T_inv=100,  lr=0.019908763029878117, damping=0.09398758455968932, 
-                          weight_decay=0, N=len(trainloader))
+                          weight_decay=0, N=len(trainloader), precision=args.precision)
                               #lr=args.learning_rate,
                               ##momentum=args.momentum,
                               #stat_decay=args.stat_decay,
@@ -158,7 +121,8 @@ def train(epoch, optimizer, net):
         #preds = (torch.nn.functional.softmax(outputs.cpu().data, dim=1), 1).squeeze()
         #print(outputs[0])
   
-        loss = pac_bayes_loss2(outputs, targets)
+        #loss = pac_bayes_loss2(outputs, targets)
+        loss = bce_loss(outputs[0], targets)
 
         optimizer.acc_stats = True
         
@@ -169,7 +133,7 @@ def train(epoch, optimizer, net):
             with torch.no_grad():
                 sampled_y = torch.multinomial(torch.nn.functional.softmax(outputs[0].data, dim=1),
                                               1).squeeze().float()
-            loss_sample = pac_bayes_loss2(outputs, sampled_y.unsqueeze(1))
+            #loss_sample = pac_bayes_loss2(outputs, sampled_y.unsqueeze(1))
             loss_sample = bce_loss(outputs[0], sampled_y.unsqueeze(1))
             loss_sample.backward(retain_graph=True)
             optimizer.acc_stats = False

utils/pac_bayes_loss.py
@@ -16,7 +16,7 @@ def pac_bayes_loss(outputs, labels, m, b, c, pi, delta):
     # TODO: the paper suggests a slightly different BRE
     val = torch.min(torch.sqrt(0.5 * BRE), BRE + torch.sqrt(BRE * (BRE + 2* bce_loss(preds, labels))))
 
-    print("KL:", kl)
+    #print("KL:", kl)
     # weak bound on KL^{-1}
     return bce_loss(preds, labels) + val
 

utils/sampling.py
@@ -87,7 +87,7 @@ def sample_from_kron_dist(q_mu, A, G, epsilon=1e-2):
     return samples
 
 
-def current_sampling(q_mu, A_inv, G_inv, epsilon=1e-2):
+def current_sampling(q_mu, A_inv, G_inv, epsilon=1e-2, precision="float32"):
     """
     Sample from a multivariate normal distribution
         N(q_mu, G^{-1} ⊗ A^{-1})
@@ -128,7 +128,9 @@ def current_sampling(q_mu, A_inv, G_inv, epsilon=1e-2):
     sqrt_G_inv = Q_G @ torch.diag(torch.sqrt(d_G)) @ Q_G.t()
     
     # Draw a standard normal sample Z of shape (m, n).
-    Z = torch.randn(m, n, device=A_inv.device)
+    Z = torch.randn(m, n, device=A_inv.device, dtype=getattr(torch, precision))
+    sqrt_A_inv, sqrt_G_inv = sqrt_A_inv.type(getattr(torch, precision)), sqrt_G_inv.type(getattr(torch, precision))
+    
     
     # Sample from the matrix normal:
     #   X = sqrt_G_inv @ Z @ sqrt_A_inv^T